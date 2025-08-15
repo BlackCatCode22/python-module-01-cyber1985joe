b158b2c3bf28a1f8cd68279dc7adabe9b34912a0
cyber1985joe <cyberjoe85@outlook.com>
2025-08-15 16:14:26 -0700

    Add files via upload

---
+++ python_test01.py
@@ -0,0 +1,13 @@
+# python_test01.py
+# JA 8/15/25
+
+print("Welcome to python_test01.py")
+
+num1 = 2
+num2 = 4
+someofnums = num1 + num2
+print("")
+print("the sum of " + str(num1) + " and " + str(num2) + " is: " + str(someofnums))
+print()
+print()
+print("end of python_test01.py")
